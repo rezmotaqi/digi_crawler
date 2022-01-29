a78a17e8aff77772a7bac8c20bf667e4eab4e166
mrm1415@gmail.com <eybabavelkon>
2022-01-29 21:29:09 +0330

    Completed crawler

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import re
-
+import asyncio
 from bs4 import BeautifulSoup
+import grequests
 import requests
+from requests import Response
 
 base_url = 'https://www.digikala.com'
 
 
-def get_links():
+def get_links() -> list:
+    """
+    return list of links for 36 most viewed smart phones
+    """
     res = requests.get('https://www.digikala.com/search/category-mobile-phone/')
     soup = BeautifulSoup(res.text, 'lxml')
     ul = soup.find("ul", attrs={"class": "c-listing__items js-plp-products-list"})
@@ -14,19 +19,19 @@ def get_links():
     links = []
     for a in a_tags:
         links.append(a.attrs['href'])
-
     return links
 
 
-async def get_product_info(product_link):
-    """
-    gets product link and returns object of dict type containing data for given link
-    """
-    res = requests.get(f'{base_url}' + f'{product_link}')
+def get_product_info(link: str) -> Response:
+    res = requests.get(f'{base_url}' + f'{link}')
+    return res
+
+
+def parse_product_info(res: Response) -> dict:
     soup = BeautifulSoup(res.text, 'lxml')
     product_name = soup.find('div', class_='c-product__title-container').find('h1').text.replace('\n', '').strip()
-    final_price = soup.find('div', class_='c-product__seller-price-pure js-price-value').text.replace('\n', '').strip()
-
+    final_price = soup.find('div', class_='c-product__seller-price-pure js-price-value').text.replace('\n',
+                                                                                                      '').strip()
     discount_price = soup.find('div', class_='c-product__seller-price-info') \
         .find('div', class_='c-product__seller-price-prev js-rrp-price')
     if discount_price is not None:
@@ -46,7 +51,7 @@ async def get_product_info(product_link):
         ratings[key] = value
 
     data = {
-        "product_link": product_link,
+        "product_link": res.url,
         "product_name": product_name,
         "price": final_price,
         "discount_price": discount_price,
@@ -54,16 +59,17 @@ async def get_product_info(product_link):
         "ratings": ratings
 
     }
+
     return data
 
 
-
 def main():
-    for product_link in get_links():
-        data = get_product_info(product_link)
+    links = get_links()
+    for link in links:
+        res = get_product_info(link)
+        data = parse_product_info(res)
         print(data)
 
 
 if __name__ == '__main__':
     main()
-
